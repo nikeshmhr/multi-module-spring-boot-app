3fe0f1052fc29b442199acdffebbcd2eecd1bf19
Nikesh Maharjan <nikeshmhr@gmail.com>
2017-01-31 10:31:59 +0545

    Changes

--- entities/src/main/java/com/f1soft/entities/Employee.java
+++ entities/src/main/java/com/f1soft/entities/Employee.java
@@ -1,11 +1,15 @@
 package com.f1soft.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
